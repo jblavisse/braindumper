86aebf776f0b1480071d0d9ce9301fe3ac68be60
Emma TREMLET <elecuelle001@ensc.fr>
2023-11-14 16:13:42 +0100

    feat: :sparkles: form which add a new memory

--- src/Controller/MemoryController.php
+++ src/Controller/MemoryController.php
@@ -6,18 +6,48 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\MemoryRepository;
+use App\Entity\Memory;
+use App\Form\AddMemoryType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MemoryController extends AbstractController
 {
-    
+
     #[Route('/', name: 'app_memory')]
     public function show(MemoryRepository $memoryRepository): Response
     {
         $memory = $memoryRepository
             ->findAll();
 
-
         return $this->render('memory/memory.html.twig', ['donnees' => $memory]);
 
+    }
+
+    private $entityManager;
+    private $taskRepository;
+    #[Route('/form', name: 'add_memory')]
+    public function __construct(EntityManagerInterface $entityManager, MemoryRepository $memoryRepository)
+    {
+        $this->entityManager = $entityManager;
+        $this->memoryRepository = $memoryRepository;
+    }
+
+    public function addMemory(Request $request): Response
+    {
+        $memory = new Memory();
+        $form = $this->createForm(AddMemoryType::class, $memory);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($memory);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('memory_list');
+        }
+
+        return $this->render('memory/form.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
